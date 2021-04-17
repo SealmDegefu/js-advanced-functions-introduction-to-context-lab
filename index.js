20940991a25b8330925aa3cfc145b7630347e783
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-17 15:40:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents:[],
+        timeOutEvents: [],
+    }
+}
+
+// }
+// function createEmployeeRecords(updateRow){
+//    return updateRow.map(data =>{
+//         return createEmployeeRecord(data)
+//     })
+    
+// }
+
+// let createTimeInEvent = function(employee, dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+//     employee.timeInEvents.push({
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+//     return employee
+// }
