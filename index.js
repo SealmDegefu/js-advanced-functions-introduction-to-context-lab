738082bea76e119db370c06e734b07c58a88b85a
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-17 16:56:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -10,20 +10,40 @@ let createEmployeeRecord = function(row){
     }
 }
 
-// }
-// function createEmployeeRecords(updateRow){
-//    return updateRow.map(data =>{
-//         return createEmployeeRecord(data)
-//     })
+function createEmployeeRecords(updateRow){
+   return updateRow.map(data =>{
+        return createEmployeeRecord(data)
+    })
     
-// }
+}
 
-// let createTimeInEvent = function(employee, dateStamp){
-//     let [date, hour] = dateStamp.split(' ')
-//     employee.timeInEvents.push({
-//         type: "TimeIn",
-//         hour: parseInt(hour, 10),
-//         date,
-//     })
-//     return employee
-// }
+let createTimeInEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+}
+
+let createTimeOutEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employee
+}
+
+let hoursWorkedOnDate = function(employee,workedHour){
+    let eventIn = employee.TimeInEvent.find(function (e){
+        return e.date = workedHour
+    })
+    let eventOut = employee.timeOutEvents.find(function (e){
+        return e.date = workedHour
+    })
+
+    return (eventOut.hour - eventIn.hour)/100
+}
